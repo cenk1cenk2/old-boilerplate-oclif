560fe96b47bf4acce21d1378fd3bb91c21b05b75
Cenk Kılıç <cenk@kilic.dev>
2020-07-02 12:47:52 +0200

    fix: private package

--- release.config.js
+++ release.config.js
@@ -7,22 +7,18 @@ module.exports = {
     "@semantic-release/changelog",
     "@semantic-release/git"
   ],
-  "plugins": [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
+  "prepare": [
     "@semantic-release/changelog",
-    [
-      "@semantic-release/git",
-      {
-        "assets": [
-          "**/CHANGELOG.md",
-          process.env.README_LOCATION ? process.env.README_LOCATION : 'README.md',
-          "yarn.lock",
-          "npm-shrinkwrap.json"
-        ],
-        "message": "chore(release): <%= nextRelease.version %> - <%= new Date().toISOString().slice(0,10).replace(/-/g,'') %> [skip ci]\n\n<%= nextRelease.notes %>"
-      }
-    ],
-    "@semantic-release/npm"
+    {
+      "path": "@semantic-release/git",
+      "assets": [
+        "CHANGELOG.md",
+        "packages/*/CHANGELOG.md",
+        process.env.README_LOCATION ? process.env.README_LOCATION : 'README.md' ,
+        "yarn.lock",
+        "npm-shrinkwrap.json"
+      ],
+      "message": "chore(release): <%= nextRelease.version %> - <%= new Date().toISOString().slice(0,10).replace(/-/g,'') %> [skip ci]\n\n<%= nextRelease.notes %>"
+    }
   ]
 }
