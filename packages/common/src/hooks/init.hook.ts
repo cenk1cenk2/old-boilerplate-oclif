2c26a8a53b38669f8757b801c674025579e078a3
Cenk Kılıç <cenk@kilic.dev>
2020-08-29 15:40:45 +0200

    fix(hook): update hook function to have opts

--- packages/common/src/hooks/init.hook.ts
+++ packages/common/src/hooks/init.hook.ts
@@ -1,9 +1,13 @@
 import { Hook } from '@oclif/config'
+import { HookKeyOrOptions } from '@oclif/config/lib/hooks'
 import config from 'config'
 
 import { Logger } from '@extend/logger'
 
-export function generateInitHook (options: { defaultTask?: () => Promise<void>, preliminaryTask?: () => Promise<void>, logo: (version: string) => string }): Hook<'init'> {
+export function generateInitHook (options: {
+  defaultTask?: (opts?: HookKeyOrOptions<any>) => Promise<void>
+  preliminaryTask?: (opts?: HookKeyOrOptions<any>) => Promise<void>
+  logo: (version: string) => string }): Hook<'init'> {
   const initHook: Hook<'init'> = async (opts): Promise<void> => {
     // initiate logger
     const logger = new Logger(opts.config.name).log
@@ -14,13 +18,13 @@ export function generateInitHook (options: { defaultTask?: () => Promise<void>, 
     }
 
     if (options.preliminaryTask) {
-      await options.preliminaryTask()
+      await options.preliminaryTask(opts)
     }
 
     // run default command
     if (options?.defaultTask && !opts.id) {
       try {
-        await options.defaultTask()
+        await options.defaultTask(opts)
       } finally {
         process.exit(0)
       }
