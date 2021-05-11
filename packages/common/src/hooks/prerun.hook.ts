8a9c49a3b807ffcc9316f96ee714354855a1ce1d
Cenk Kılıç <cenk@kilic.dev>
2021-05-11 14:34:44 +0200

    feat: update prerunhook to have options for work

--- packages/common/src/hooks/prerun.hook.ts
+++ packages/common/src/hooks/prerun.hook.ts
@@ -4,23 +4,30 @@ import { createInterface } from 'readline'
 
 import { Logger } from '@extend/logger'
 
-export const prerunHook: Hook<'prerun'> = async () => {
-  // graceful terminate
-  if (os.platform() === 'win32') {
-    createInterface({
-      input: process.stdin,
-      output: process.stdout
-    }).on('SIGINT', () => {
-      process.kill(process.pid, 'SIGINT')
-    })
+export function prerunHookWithOptions (options?: { registerExitListeners?: boolean }): Hook<'prerun'> {
+  return async (): Promise<void> => {
+    // graceful terminate
+    if (os.platform() === 'win32') {
+      createInterface({
+        input: process.stdin,
+        output: process.stdout
+      }).on('SIGINT', () => {
+        process.kill(process.pid, 'SIGINT')
+      })
+    }
+
+    if (options?.registerExitListeners !== false) {
+      process.on('SIGINT', () => {
+        const logger = new Logger().log
+
+        // show that we have understood that
+        logger.fatal('Caught terminate signal.', { custom: 'exit' })
+
+        process.exit(127)
+      })
+    }
   }
-
-  process.on('SIGINT', () => {
-    const logger = new Logger().log
-
-    // show that we have understood that
-    logger.fail('Caught terminate signal.', { custom: 'exit' })
-
-    process.exit(127)
-  })
 }
+
+/** @deprecated Legacy version to not break compatibility */
+export const prerunHook: Hook<'prerun'> = prerunHookWithOptions({ registerExitListeners: true })
